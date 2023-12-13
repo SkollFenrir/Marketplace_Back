e201251af366644ff6d9d407f82a19d5416fa711
Diego H <diegoadonis2@gmail.com>
2023-12-13 00:23:28 -0300

    myProducts operativo

--- query.js
+++ query.js
@@ -10,6 +10,17 @@ const pool = new Pool({
 	allowExitOnIdle: true,
 });
 
+const getUsuarioId = async (correoUsuario) =>{
+	try {
+		const formatQuery = format('SELECT id FROM usuarios WHERE correo = %L', correoUsuario)
+		const {rows: [{id}]} = await pool.query(formatQuery)
+		return id
+	} catch (error) {
+		console.error('Error al verificar la existencia del producto:', error);
+		throw { code: 404, message: 'No se encontró ningún usuario con ese CORREO' };
+	}
+}
+
 const getProducts = async () => {
 	const { rows: productos } = await pool.query('SELECT * FROM productos;');
 	return productos;
@@ -21,12 +32,20 @@ const getProduct = async (id) => {
 	return producto;
 };
 
-const getMyProducts = async () => {};
-
-const postVender = async (producto) => {
-	let { usuario_id, titulo, descripcion, url_img, precio, estado } = producto;
+const getMyProducts = async (correoUsuario) => {
 	const formatQuery = format(
-		'INSERT INTO productos VALUES (DEFAULT, %s, %L, %L, %L, %s, %L)',
+		'SELECT * FROM productos WHERE usuario_id = (SELECT id FROM usuarios WHERE correo = %L)',
+		correoUsuario
+	);
+	const { rows: myProducts } = await pool.query(formatQuery);
+	return myProducts;
+};
+
+const postVender = async (producto, correoUsuario) => {
+	let {  titulo, descripcion, url_img, precio, estado } = producto;
+	const usuario_id = await getUsuarioId(correoUsuario)
+	const formatQuery = format(
+		'INSERT INTO productos (id, usuario_id, titulo, descripcion, url_img, precio, estado) VALUES (DEFAULT, %s, %L, %L, %L, %s, %L)',
 		usuario_id,
 		titulo,
 		descripcion,
@@ -57,7 +76,7 @@ const postRegistrarU = async (usuario) => {
 	let { nombre, apellido, correo, contrasena, genero } = usuario;
 	const contrasenaCrypt = bcrypt.hashSync(contrasena, saltRounds);
 	const formatQuery = format(
-		'INSERT INTO usuarios VALUES (DEFAULT, %L, %L, %L, %L, %L)',
+		'INSERT INTO usuarios (id, nombre, apellido, correo, contrasena, genero) VALUES (DEFAULT, %L, %L, %L, %L, %L)',
 		nombre,
 		apellido,
 		correo,
@@ -69,14 +88,17 @@ const postRegistrarU = async (usuario) => {
 
 const checkProductExists = async (productId) => {
 	try {
-		const formatQuery = format('SELECT 1 FROM productos WHERE id = %s', productId)
-	  const result = await pool.query(formatQuery);
-	  return result.rows.length > 0;
+		const formatQuery = format(
+			'SELECT 1 FROM productos WHERE id = %s',
+			productId
+		);
+		const result = await pool.query(formatQuery);
+		return result.rows.length > 0;
 	} catch (error) {
-	  console.error('Error al verificar la existencia del producto:', error);
-	  throw { code: 500, message: 'Error interno del servidor' };
+		console.error('Error al verificar la existencia del producto:', error);
+		throw { code: 500, message: 'Error interno del servidor' };
 	}
-  };
+};
 
 const updateProducto = async (producto, id) => {
 	let { precio, estado } = producto;
@@ -86,18 +108,18 @@ const updateProducto = async (producto, id) => {
 		estado,
 		id
 	);
-	const productExists = await checkProductExists(id)
+	const productExists = await checkProductExists(id);
 	if (!productExists) {
 		throw { code: 404, message: 'No se encontró ningún producto con ese ID' };
-	  }
-	await pool.query(formatQuery)
+	}
+	await pool.query(formatQuery);
 };
 
 const deleteProduct = async (id) => {
-	const productExists = await checkProductExists(id)
+	const productExists = await checkProductExists(id);
 	if (!productExists) {
 		throw { code: 404, message: 'No se encontró ningún producto con ese ID' };
-	  }
+	}
 	const formatQuery = format('DELETE FROM productos WHERE id = %s', id);
 	await pool.query(formatQuery);
 };
@@ -110,4 +132,5 @@ module.exports = {
 	postVender,
 	updateProducto,
 	deleteProduct,
+	getMyProducts,
 };
